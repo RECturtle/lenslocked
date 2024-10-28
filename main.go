3ed5560d3673b5b5a24cd91087fb9e705d038f02
RECturtle <spencer.mertes@gmail.com>
2024-10-28 08:05:41 -0400

    feat: add templates for contact and faq pages

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := template.ParseFiles("templates/home.gohtml")
+	tpl, err := template.ParseFiles(filepath)
 
 	if err != nil {
 		log.Printf("Error Parsing Template: %v", err)
@@ -28,30 +28,16 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	executeTemplate(w, "templates/home.gohtml")
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:spencer.mertes@gmail.com\">spencer.mertes@gmail.com</a>.</p>")
+	executeTemplate(w, "templates/contact.gohtml")
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>FAQ Page</h1>
-	<ul>
-	  <li>
-		<b>Is there a free version?</b>
-		Yes! We offer a free trial for 30 days on any paid plans.
-	  </li>
-	  <li>
-		<b>What are your support hours?</b>
-		We have support staff answering emails 24/7, though response
-		times may be a bit slower on weekends.
-	  </li>
-	  <li>
-		<b>How do I contact support?</b>
-		Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
-	  </li>
-	</ul>
-	`)
+	executeTemplate(w, "templates/faq.gohtml")
 }
 
 func main() {
